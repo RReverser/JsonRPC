0815d433cebe4f8c8765229ae0eb6b60cd0ff5eb
Igor Reverser <RReverser@gmail.com>
2012-11-19 14:26:11 +0200

    Added assembly information and NuGet package.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JsonRPC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("JsonRPC Portable Class Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("RReverser")]
 [assembly: AssemblyProduct("JsonRPC")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
